89226352e687728d8b364978c4d7b10954277889
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2018-12-06 15:42:14 +0300

    Initial Commit

---
+++ FacebookNewsfeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookNewsfeed
+//
+//  Created by Алексей Пархоменко on 06/12/2018.
+//  Copyright © 2018 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
